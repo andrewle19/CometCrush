9d514f45a7fa1811c3371ec48a60ba23e585ae9b
= <=>
2017-05-09 03:22:25 -0700

    Added Executables for mac TODO: powerups/boss/windows exe

--- setupWin.py
+++ setupWin.py
@@ -3,12 +3,12 @@ from setuptools import setup
 APP = ['main.py']
 
 DATA_FILES = [('',['img']),('',['sound']),('high_score.txt')]
-OPTIONS = {'iconfile':'app.icns',}
+OPTIONS = {'iconfile':'app.ico',}
 setup(
 
   app = APP,
   data_files = DATA_FILES,
-  options = {'py2app': OPTIONS},
-  setup_requires = ['py2app'],
+  options = {'py2exe': OPTIONS},
+  setup_requires = ['py2exe'],
 
 )
